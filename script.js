cbba54a557a995638c708116d9dbcc98e634acc3
Nico-Mayer <nico.m94@icloud.com>
2021-01-26 17:29:27 +0100

    burger btn

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function test(){
     alert("Test");
 }
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
