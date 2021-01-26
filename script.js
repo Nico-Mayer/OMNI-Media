9728a3faa1734e0e492513d9008bb573b8101f42
Nico-Mayer <nico.m94@icloud.com>
2021-01-26 23:00:18 +0100

    some menu changes

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ function test(){
 
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
+const navbar = document.querySelector('.nav-bar');
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    navbar.classList.add('open_menu');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    navbar.classList.remove('open_menu');
     menuOpen = false;
   }
 });
